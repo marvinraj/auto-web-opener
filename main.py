c2deff4c20c758d11047ded0d2f57d4e3db432fe
Marvin <marvinraj77@gmail.com>
2024-02-14 11:39:10 +0800

    added appropriate comments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import webbrowser
 
-# using webbrowser module
+# using webbrowser module to open web urls
 url_list = ["https://techcrunch.com/", "https://www.theverge.com/", "https://www.zdnet.com/#google_vignette", "https://medium.com/"]
 
 for i in url_list:
     webbrowser.open_new(i)
+
+# scheduling feature
+    
+
+# store progress of running the code/reading articles everyday
